d90926c68f4cb2094264f57937825a616d931b9f
Edgar Chambilla <edgar.chambilla@utec.edu.pe>
2024-06-01 18:16:04 -0500

    asd

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import {Inter, Lusitana} from 'next/font/google'
+
+export const inter = Inter({subsets: ['latin']})
+export const lusitana = Lusitana({weight:["400","700"],subsets:['latin']})
+
+// weight: "400" | "700" | ("400" | "700")[];
+//     style?: "normal" | "normal"[] | undefined;
+//     display?: Display | undefined;
+//     variable?: undefined;
+//     preload?: boolean | undefined;
+//     fallback?: string[] | undefined;
+//     adjustFontFallback?: boolean | undefined;
+//     subsets?: "latin"[] | undefined;
